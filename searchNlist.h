c630793499994501b3aa0cf9ea5c7bad4fbbfabe
nvirdi2 <71736420+nvirdi2@users.noreply.github.com>
2021-03-31 18:15:52 -0400

    Update searchNlist.h

--- searchNlist.h
+++ searchNlist.h
@@ -1,3 +1,10 @@
+// Name: Navdeep Virdi
+// Seneca email: nvirdi2@myseneca.ca
+// Student ID: 166485193
+// Date: Feb 18th, 2021
+
+//I have done all the coding by myself and only copied the code that my professor provided to complete my workshops and assignments.
+
 #ifndef SDDS_SEARCHNLIST_H_
 #define SDDS_SEARCHNLIST_H_
 
